ce4e06c1c7633f71d5be5f73f1fe45d8cbbbcae1
Andrei Miashkou <meshkow.andrei@gmail.com>
2021-06-23 11:21:16 +0300

    feat: add katas

---
+++ 7kyu/Folding your way to the moon.py
@@ -0,0 +1,47 @@
+# Folding your way to the moon
+#
+# https://www.codewars.com/kata/58f0ba42e89aa6158400000e
+#
+# Have you heard about the myth that if you fold a paper enough times, you can reach the moon with it? Sure you have, but exactly how many? Maybe it's time to write a program to figure it out.
+#
+# You know that a piece of paper has a thickness of 0.0001m. Given distance in units of meters, calculate how many times you have to fold the paper to make the paper reach this distance.
+# (If you're not familiar with the concept of folding a paper: Each fold doubles its total thickness.)
+#
+# Note: Of course you can't do half a fold. You should know what this means ;P
+#
+# Also, if somebody is giving you a negative distance, it's clearly bogus and you should yell at them by returning null (or whatever equivalent in your language. In Shell please return None).
+
+def fold_to(distance):
+    thickness = 0.0001
+    count = 0
+    if distance < 0:
+        return None
+    elif distance <= thickness:
+        return 0
+    while distance > thickness:
+        count += 1
+        thickness *= 2
+    return count
+
+# Best practice:
+#
+#
+# def fold_to(distance, thickness=0.0001, folds=0):
+#     if distance < 0:
+#         return
+#
+#     while thickness < distance:
+#         thickness *= 2
+#         folds += 1
+#
+#     return folds
+#
+# Clever solution:
+#
+# import math
+# def fold_to(d):
+#     if d<0:
+#         return None
+#     if d<0.0001:
+#         return 0
+#     return math.ceil(math.log(d/0.0001,2))
