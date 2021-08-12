3c7c23bb09afff920ffe4919725be75e9e1631c1
Andrei Miashkou <meshkow.andrei@gmail.com>
2021-08-12 20:01:07 +0300

    feat: add kata

---
+++ 8kyu/Convert a Number to a String!.py
@@ -0,0 +1,23 @@
+# Convert a Number to a String!
+#
+# https://www.codewars.com/kata/5265326f5fda8eb1160004c8
+#
+# We need a function that can transform a number into a string.
+#
+# What ways of achieving this do you know?
+#
+# Examples:
+# 123 --> "123"
+# 999 --> "999"
+
+def number_to_string(num):
+    return str(num)
+
+# Best practice:
+#
+# def number_to_string(num):
+#     return str(num)
+#
+# Clever solution:
+#
+# number_to_string = str
