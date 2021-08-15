cdfc32c91997b5cfd4374371abefa3a2aa02ab93
Andrei Miashkou <meshkow.andrei@gmail.com>
2021-08-15 20:57:48 +0300

    feat: add kata

---
+++ 8kyu/Remove String Spaces.py
@@ -0,0 +1,13 @@
+# Remove String Spaces
+#
+# https://www.codewars.com/kata/57eae20f5500ad98e50002c5
+#
+# Simple, remove the spaces from the string, then return the resultant string.
+
+def no_space(x):
+    return x.replace(" ", "")
+
+# Best practice and clever solution:
+#
+# def no_space(x):
+#     return x.replace(' ' ,'')
