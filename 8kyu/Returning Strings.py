370c0f6ba4da35efdf923e06adecbdb3faa2ca22
Andrei Miashkou <meshkow.andrei@gmail.com>
2021-08-25 15:20:41 +0300

    feat: add kata

---
+++ 8kyu/Returning Strings.py
@@ -0,0 +1,15 @@
+# Returning Strings
+#
+# https://www.codewars.com/kata/55a70521798b14d4750000a4
+#
+# Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+#
+# [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+def greet(name):
+    return f"Hello, {name} how are you doing today?"
+
+# Best practice and clever solution:
+#
+# def greet(name):
+#     return f'Hello, {name} how are you doing today?'
