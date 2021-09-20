6521c223b4f9a8b4cdbeb1fa12f5be233a030c1d
Andrei Miashkou <meshkow.andrei@gmail.com>
2021-09-20 19:39:53 +0300

    feat: add kata

---
+++ 8kyu/Beginner - Reduce but Grow.py
@@ -0,0 +1,20 @@
+# Beginner - Reduce but Grow
+#
+# https://www.codewars.com/kata/57f780909f7e8e3183000078
+#
+# Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+#
+# [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+import math
+
+
+def grow(arr):
+    return math.prod(arr)
+
+# Best practice and clever solution:
+#
+# from functools import reduce
+#
+# def grow(arr):
+#     return reduce(lambda x, y: x * y, arr)
