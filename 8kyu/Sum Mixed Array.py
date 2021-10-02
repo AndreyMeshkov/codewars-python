5b6d1b5b838a65f781fb42980fc7b57ebb4cfb24
Andrei Miashkou <meshkow.andrei@gmail.com>
2021-10-02 18:10:46 +0300

    feat: add kata

---
+++ 8kyu/Sum Mixed Array.py
@@ -0,0 +1,15 @@
+# Sum Mixed Array
+#
+# https://www.codewars.com/kata/57eaeb9578748ff92a000009
+#
+# Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+#
+# Return your answer as a number.
+
+def sum_mix(arr):
+    return sum(int(x) for x in arr)
+
+# Best practice and clever solution:
+#
+# def sum_mix(arr):
+#     return sum(map(int, arr))
